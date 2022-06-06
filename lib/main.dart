4f7620bc996e952372c5517b23b0f2940b7ae55e
kibangkibang <csh0989@naver.com>
2022-06-06 18:36:33 +0900

    로그인 화면 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/screens/screen_detail.dart';
 import 'package:flutter_shopping_mall/screens/screen_index.dart';
 import 'package:flutter_shopping_mall/screens/screen_login.dart';
+import 'package:flutter_shopping_mall/screens/screen_register.dart';
+import 'package:flutter_shopping_mall/screens/screen_search.dart';
 import 'package:flutter_shopping_mall/screens/screen_splash.dart';
 
-void main(){
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -18,8 +26,14 @@ class MyApp extends StatelessWidget {
         '/':(context) => SplashScreen(),
         '/index':(context) => IndexScreen(),
         '/login':(context) => LoginScreen(),
+        '/register':(context) => RegisterScreen(),
+        '/detail':(context) => DetailScreen(),
+        '/search':(context) => SearchScreen(),
       },
       initialRoute: '/',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
     );
   }
 }
---
+++ lib/screens/screen_register.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Register'),
+      ),
+    );
+  }
+}
