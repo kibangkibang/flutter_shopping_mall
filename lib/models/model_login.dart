e8f6b8c064397eb4fd993f4b6995a772a3896c02
kibangkibang <csh0989@naver.com>
2022-06-09 20:27:34 +0900

    로그인기능 구현

---
+++ lib/models/model_login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginFieldModel extends ChangeNotifier{
+  String email ="";
+  String password ="";
+
+  void setEmail(String email){
+    this.email = email;
+    notifyListeners();
+  }
+
+  void setPassword(String password){
+    this.password = password;
+    notifyListeners();
+  }
+
+}
--- lib/screens/screen_login.dart
+++ lib/screens/screen_login.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_shopping_mall/models/model_auth.dart';
+import 'package:flutter_shopping_mall/models/model_login.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('로그인'),
+    return ChangeNotifierProvider(
+      create: (_) => LoginFieldModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('로그인'),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            EmailInput(),
+            PasswordInput(),
+            LoginButton(),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Divider(thickness: 1),
+            ),
+            RegisterButton(),
+          ],
+        )),
       ),
-      body: Center(
-          child: Column(
-        children: [
-          EmailInput(),
-          PasswordInput(),
-          LoginButton(),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Divider(thickness: 1),
-          ),
-          RegisterButton(),
-        ],
-      )),
     );
   }
 }
@@ -28,10 +34,13 @@ class LoginScreen extends StatelessWidget {
 class EmailInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginField = Provider.of<LoginFieldModel>(listen: false, context);
     return Container(
       padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: TextField(
-        onChanged: (email) {},
+        onChanged: (email) {
+          loginField.setEmail(email);
+        },
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(labelText: '이메일', helperText: ''),
       ),
@@ -42,10 +51,13 @@ class EmailInput extends StatelessWidget {
 class PasswordInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginField = Provider.of<LoginFieldModel>(listen: false, context);
     return Container(
       padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: TextField(
-        onChanged: (password) {},
+        onChanged: (password) {
+          loginField.setPassword(password);
+        },
         obscureText: true,
         decoration: InputDecoration(
           labelText: '비밀번호',
@@ -59,30 +71,54 @@ class PasswordInput extends StatelessWidget {
 class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final authClient =
+        Provider.of<FirebaseAuthProvider>(listen: false, context);
+    final loginField = Provider.of<LoginFieldModel>(listen: false, context);
+
     return Container(
       width: MediaQuery.of(context).size.width * 0.85,
       height: MediaQuery.of(context).size.height * 0.05,
       child: ElevatedButton(
         child: Text('로그인'),
-        onPressed: (){},
+        onPressed: () async {
+          await authClient
+              .loginWithEmail(loginField.email, loginField.password)
+              .then((status) {
+            if (status == AuthStatus.loginSuccess) {
+              Scaffold.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(SnackBar(
+                    content:
+                        Text(authClient.user!.email!.toString() + '님 환영합니다.')));
+                        Navigator.pushReplacementNamed(context,'/index');
+            }else{
+              Scaffold.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                    SnackBar(content: Text('로그인에 실패했습니다. 다시 시도해주세요.')));
+            }
+          });
+        },
         style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0)
-          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
         ),
       ),
     );
   }
 }
-class RegisterButton extends StatelessWidget {
 
+class RegisterButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return TextButton(onPressed: () {
-      Navigator.of(context).pushNamed('/register');
-    }, child: Text('이메일로 간단하게 회원가입하기',style: TextStyle(
-      color: theme.primaryColor
-    ),));
+    return TextButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed('/register');
+        },
+        child: Text(
+          '이메일로 간단하게 회원가입하기',
+          style: TextStyle(color: theme.primaryColor),
+        ));
   }
 }
