da480ef9d3b59c2c9434216e2b886140a26b7b98
kibangkibang <csh0989@naver.com>
2022-06-09 20:50:40 +0900

    로그인기능 Splash 자동로그인 구현

--- lib/screens/screen_splash.dart
+++ lib/screens/screen_splash.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/models/model_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,8 +13,23 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   Future<bool> checkLogin() async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    bool isLogin = pref.getBool('isLogin') ?? false;
+    final authClient = Provider.of<FirebaseAuthProvider>(listen: false,context);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = prefs.getBool('isLogin') ?? false;
+    print('[*] 로그인 상태 : ' + isLogin.toString());
+    if(isLogin){
+      String? email = prefs.getString('email');
+      String? password = prefs.getString('password');
+      await authClient.loginWithEmail(email!, password!).then((status){
+        if(status == AuthStatus.loginSuccess){
+          print('[+] 로그인성공');
+        }else{
+          print('[-] 로그인실패');
+          isLogin = false;
+          prefs.setBool('isLogin', isLogin);
+        }
+      });
+    }
     return isLogin;
   }
 
@@ -32,6 +49,11 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(Duration(milliseconds: 1500), () {
       moveScreen();
     },);
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
