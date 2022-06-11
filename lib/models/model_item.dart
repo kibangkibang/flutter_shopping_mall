0052c526ed6a9c2664b3885705534774b8d81511
kibangkibang <csh0989@naver.com>
2022-06-11 19:00:29 +0900

    아이템 모델 작성

---
+++ lib/models/model_item.dart
@@ -0,0 +1,19 @@
+class Item{
+  late String title;
+  late String description;
+  late String brand;
+  late String imageUrl;
+  late int price;
+  late String registerDate;
+  late String id;
+
+  Item({
+    required this.title,
+    required this.description,
+    required this.brand,
+    required this.imageUrl,
+    required this.price,
+    required this.registerDate,
+    required this.id
+  });
+}
